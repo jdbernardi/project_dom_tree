0c452643178254c1e25eb87173c25bb64b21fd8a
Joe Bernardi <joebernardi@hotmail.com>
2016-08-31 22:09:16 -0500

    work on closing

--- lib/render.rb
+++ lib/render.rb
@@ -2,19 +2,36 @@ require 'pry'
 
 class Render
 
+	def initialize
+
+		@stack = []
+
+	end
+
+
+
 	def render( root )
 
 		current_node = root
 
+
+
 		return if current_node.children.nil?
 
-		current_node.children.each do | child |
+
+	  current_node.children.each_with_index do | child, index |
 
 			print "<#{child.tag}>" unless !child.tag
 
 			print "#{child.content}"
 
 			print "</#{child.tag}>" if child.children.empty?
+
+#				if index == current_node.children.count - 1
+
+#					puts "</#{current_node.tag}>"
+
+#			  end
 
 			puts ''
 
