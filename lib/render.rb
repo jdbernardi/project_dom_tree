1b313e6c3cdbfd9763885dd89f42e4470a74840d
Joe Bernardi <joebernardi@hotmail.com>
2016-08-31 22:33:37 -0500

    revert

--- lib/render.rb
+++ lib/render.rb
@@ -14,24 +14,20 @@ class Render
 
 		current_node = root
 
-
+		@stack << current_node
 
 		return if current_node.children.nil?
 
+# if we are at the last child of the parent
+# we print the parents closing tag
 
 	  current_node.children.each_with_index do | child, index |
 
+
 			print "<#{child.tag}>" unless !child.tag
-
 			print "#{child.content}"
-
 			print "</#{child.tag}>" if child.children.empty?
 
-#				if index == current_node.children.count - 1
-
-#					puts "</#{current_node.tag}>"
-
-#			  end
 
 			puts ''
 
@@ -40,8 +36,9 @@ class Render
 		end
 
 
-
 	end #/.render
+
+
 
 
 
