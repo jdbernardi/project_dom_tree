fc2c1a7acde889ac892d5870e7166eec0057f13c
Joe Bernardi <joebernardi@hotmail.com>
2016-09-16 15:20:42 -0500

    refactor

--- lib/render.rb
+++ lib/render.rb
@@ -7,14 +7,14 @@ class Render
 
 
 		@open_tag_spacing = 0
-		@closing_tag_spacing = 0
+
 
 
 	end
 
-
+	# array is broken up into Nodes
 	def render_search_results( root )
-		# breaks up the passed results array into nodes to send to render
+
 		root.each do | node |
 
 			render( node )
@@ -33,8 +33,8 @@ class Render
 
 		format_render( current_node )
 
-		# then iterate through each of the children of the node
 		if current_node.children != []
+
 			current_node.children.each do | child |
 
 					render( child )
@@ -99,13 +99,12 @@ class Render
 
 			print_text( child )
 
-		# the tag will have either text or a child
 		elsif child.attributes[:text] && child.children == []
-		# if there is no text then we jump to next line
+
 			print_text( child )
 
 		elsif child.children != []
-			# if there is a child, then we jump to next line
+
 			print_text( child )
 			puts ""
 
